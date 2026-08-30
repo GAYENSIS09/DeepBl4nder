5b6cd7274abf558a019b4db045961c5f6a2c759a
GAYENSIS09 <gayensis09@gmail.com>
2026-08-30 22:19:11 +0000

    fix: Add basePath for GitHub Pages deployment

--- docs-site/next.config.js
+++ docs-site/next.config.js
@@ -3,5 +3,7 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath: '/DeepBl4nder',
+  assetPrefix: '/DeepBl4nder/',
 }
 module.exports = nextConfig
